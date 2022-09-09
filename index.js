1caeecae7c8f80aaa1f42ebb970adc3b7db46821
AmielRe <amieli1999@gmail.com>
2022-09-09 16:37:26 +0300

    Add login page and serve it on initial connection.

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const mongoose = require('mongoose')
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.json());
+app.use(express.static('public/css'))
+app.use(express.static('public/images'))
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 
 // Routers
 const ordersRoute = require('./routes/orders')
@@ -18,8 +22,8 @@ app.use('/rooms', roomsRoute)
 app.use('/users', usersRoute)
 
 // this will return the main page
-app.get('/', (req,res) => {
-    res.send("This will return the main page")
+app.get('/', (req, res) => {
+    res.sendFile('login.html', { root: __dirname + "/public/html" });
 })
 
 mongoose.connect(`mongodb+srv://${process.env.dbUser}:${process.env.dbPass}@easyhotel.6ku0zkj.mongodb.net/?retryWrites=true&w=majority`)
