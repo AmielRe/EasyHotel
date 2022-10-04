f0c2b8c1e4d6ad200074c6420ea270b174c5777b
AmielRe <amieli1999@gmail.com>
2022-10-04 13:42:20 +0300

    Add information button.
    Add information button for all room types.
    Remove html duplicates and replace with dynamic template.

--- public/js/reservation.js
+++ public/js/reservation.js
@@ -1,10 +1,16 @@
 $(() => {
+    $("div[data-col-type='col-suite']").load("suite-div.html")
+    $("div[data-col-type='col-exclusive']").load("exclusive-div.html")
+    $("div[data-col-type='col-family']").load("family-div.html")
+    $("div[data-col-type='col-standard']").load("standard-div.html")
+
     $('#deleteAllButton').click(function() {
         clearCart();
     })
 
-    $('.col').click(function() {
-        if($(this).hasClass('col-nonactive')) {
+    $('.col').click(function(event) {
+        if($(this).hasClass('col-nonactive')
+           || !$(event.target).is('div')) {
             return;
         }
 
@@ -26,6 +32,27 @@ $(() => {
         template = template.replaceAll('{type}', this.getAttribute('data-display-name'))
 
         $('.cart-table').append(template)
+    })
+
+    var exampleModal = document.getElementById('exampleModal')
+    exampleModal.addEventListener('show.bs.modal', function (event) {
+        // Button that triggered the modal
+        var button = event.relatedTarget
+        // Extract info from data-bs-* attributes
+        var displayName = button.getAttribute('data-display-name')
+        var informationText = button.getAttribute('data-information-text')
+        var roomImage = button.getAttribute('data-room-image')
+        // If necessary, you could initiate an AJAX request here
+        // and then do the updating in a callback.
+        
+        // Update the modal's content.
+        var modalTitle = exampleModal.querySelector('.modal-title')
+        var modalBodyText = exampleModal.querySelector('.modal-body-text')
+        var modalBodyImage = exampleModal.querySelector('.modal-body-image')
+
+        modalTitle.textContent = displayName
+        modalBodyText.textContent = informationText
+        modalBodyImage.setAttribute('src', roomImage)
     })
 })
 
