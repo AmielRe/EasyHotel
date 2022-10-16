bc8a9e8683bb41ca0c12f5dd3769a543c19a98f9
AmielRe <amieli1999@gmail.com>
2022-10-16 14:16:46 +0300

    Replace email verification.
    Replace email verification with external API request.

--- middleware/verifyPayment.js
+++ middleware/verifyPayment.js
@@ -1,6 +1,9 @@
+const https = require('https');
+
 const verifyPaymentForm = () => {
-    return (req, res, next) => { 
-        if(req.body.Email == null || !validateEmail(req.body.Email)) {
+    return async (req, res, next) => { 
+        const isValidEmail = await validateEmail(req.body.Email);
+        if(req.body.Email == null || !isValidEmail) {
             return res.status(400).send({
                 error: "Invalid email."
             });
@@ -31,7 +34,28 @@ const verifyCart = () => {
 }
 
 const validateEmail = (email) => {
-    return /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+    return new Promise(((resolve, reject) => {
+        const request = https.get('https://api.eva.pingutil.com/email?email=' + email, (resp) => {
+            
+          resp.setEncoding('utf8');
+          let returnData = '';
+    
+          resp.on('data', (chunk) => {
+            returnData += chunk;
+          });
+    
+          resp.on('end', () => {
+                parsedData = JSON.parse(returnData);
+                resolve(parsedData.data.valid_syntax && parsedData.data.deliverable);
+          });
+    
+          resp.on('error', (error) => {
+              resolve(false);
+          });
+        });
+        
+        request.end();
+      }));
   };
 
   module.exports = {
