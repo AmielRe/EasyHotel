df76e2f644423408df97e22030e47e2b7f4ef7a1
elad_pt <48997586+Sh3lldor@users.noreply.github.com>
2022-10-23 22:05:35 +0300

    Chat fixed landing.

--- public/js/chats/chats-admins.js
+++ public/js/chats/chats-admins.js
@@ -32,39 +32,44 @@ $(document).ready(function (){
 
     // Click on the user
     $(document).on('click','.user',function () {
-        // Clear privious chats
         $('#messages').empty();
 
         email = $(this).children($('div')).text();
-        $('.msgSender > strong').text(email);
 
-        $.ajax({
-            type: 'GET',
-            url: `/chat/${email}`,  
-            success: function(messages){
-                console.table(messages)
-                for (var i=0; i<messages.length; i++) {
-    
-                    $('#messages').append(`<div class="chat-message-right pb-4">
-                    <div>
-                        <img src="../../images/chats/user.png" class="rounded-circle mr-1" width="40" height="40">
-                        <div class="text-muted small text-nowrap mt-2">2:33 am</div>
-                    </div>
-                    <div class="flex-shrink-1 bg-light rounded py-2 px-3 mr-3">
-                        <div class="font-weight-bold mb-1">${messages[i].source}</div>
-                        ${messages[i].content}
-                    </div>
-                </div>`)
-                };
-            },
-            error: function(err){
-                console.log(err)
-            }
-        });
+        getMsg(email);
 
     });
-
+        
 });
+
+function getMsg(email) {
+    // Clear privious chats
+    $('.msgSender > strong').text(email);
+
+    $.ajax({
+        type: 'GET',
+        url: `/chat/${email}`,
+        success: function(messages){
+            console.table(messages)
+            for (var i=0; i<messages.length; i++) {
+
+                $('#messages').append(`<div class="chat-message-right pb-4">
+                <div>
+                    <img src="../../images/chats/user.png" class="rounded-circle mr-1" width="40" height="40">
+                    <div class="text-muted small text-nowrap mt-2">2:33 am</div>
+                </div>
+                <div class="flex-shrink-1 bg-light rounded py-2 px-3 mr-3">
+                    <div class="font-weight-bold mb-1">${messages[i].source}</div>
+                    ${messages[i].content}
+                </div>
+            </div>`)
+            };
+        },
+        error: function(err){
+            console.log(err)
+        }
+    });
+}
 
 
 function sendMsg() {
@@ -110,6 +115,9 @@ function getAllGuests() {
                     </div>
                 </a>`)
             };
+
+            // Set up first chat;
+            getMsg(users[0].email);
         },
         error: function(err){
             console.log(err)
