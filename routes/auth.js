37449f7b6ef7f921c10d4ebd71640a4495ab46c7
AmielRe <amieli1999@gmail.com>
2022-10-28 23:27:46 +0300

    Add css and remove OAuth.

--- routes/auth.js
+++ routes/auth.js
@@ -2,18 +2,15 @@ const express = require('express');
 const AuthController = require('../controllers/AuthController');
 const { route } = require('./orders');
 
-
 const router = express.Router();
 
-
 router.route('/basic')
-    .post(AuthController.basicLogin)
-
-router.route('OAuth')
-    .post(AuthController.oAuthLogin)
+    .post(AuthController.login)
 
 router.route('/access_token')
     .get(AuthController.getFacebookAccessToken)
 
+router.route('/logout')
+    .get(AuthController.logout)
 
 module.exports = router;
