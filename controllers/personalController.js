32a0e9883c4d3ec17b71f66027ad20e7c4ba1c75
elad_pt <48997586+Sh3lldor@users.noreply.github.com>
2022-11-18 17:19:08 +0200

    Deleted logging.

--- controllers/personalController.js
+++ controllers/personalController.js
@@ -5,7 +5,6 @@ const { getJwtDetails } = require('../middleware/verifyJWT');
 const personalSpace = (req,res) => {
     Order.find({ userEmail: getJwtDetails(req.cookies.jwt)["email"] })
     .then(orders => {
-        console.log(orders);
         res.status(200).render("../views/guest-panel.ejs", {orders, jwt: getJwtDetails(req.cookies.jwt)});
     }).catch(err => {
         res.status(500).json({err});
